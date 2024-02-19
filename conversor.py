df210bf35285fab11c139c62d117440562a3e1d6
GabrielSilvaSouza <gabrieldasilvasouza21@gmail.com>
2024-02-19 10:43:07 -0300

    tool is ready

--- conversor.py
+++ conversor.py
@@ -6,12 +6,12 @@ import os
 def download_video(url, filepath):
     yt = YouTube(url)
     video = yt.streams.get_highest_resolution()
-    video.download(filepath)
+    video.download(f'{filepath}')
     return video.default_filename
 
-def convert_to_mp3(video_name):
-    video = AudioFileClip(f'C:/Users/Gabriel/Downloads/{video_name}')
-    video.write_audiofile(f'C:/Users/Gabriel/Downloads/{video_name[:-4]}.mp3')
+def convert_to_mp3(video_name, filepath):
+    video = AudioFileClip(f'{filepath}/{video_name}')
+    video.write_audiofile(f'{filepath}/{video_name[:-4]}.mp3')
     return video_name[:-4]
 
 
--- tool.py
+++ tool.py
@@ -1,12 +1,12 @@
 from tkinter import *
-from tkinter import ttk, filedialog
+from tkinter import ttk, filedialog, Entry
 from conversor import *
 
 def assembling(*args):
     try:
         video_name = download_video(url.get(), filepath.get())
-        audio_name = convert_to_mp3(video_name)
-        os.remove(f'C:/Users/Gabriel/Downloads/{video_name}')
+        audio_name = convert_to_mp3(video_name, filepath.get())
+        os.remove(f'{filepath.get()}/{video_name}')
         return audio_name
     
     except ValueError:
@@ -15,16 +15,23 @@ def assembling(*args):
 def select_folder():
     Tk().withdraw()
     selection = filedialog.askdirectory()
-    return selection
+    entry.delete(0, 'end')
+    entry.insert(0, selection)
+    filepath.set(selection)
 
+    return selection
 
 
 
 root = Tk()
 root.title("Youtube Conveter")
 
+entry = Entry(root)
+
+
 mainframe = ttk.Frame(root, padding="3 3 200 200")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
@@ -34,13 +41,11 @@ link_entry.grid(column=2, row=1, sticky=(W, E))
 ttk.Label(mainframe, text="Link").grid(column=1, row=1, sticky=E)
 
 
-filepath = StringVar()
-label_path = ttk.Label(mainframe, textvariable=filepath).grid(column=1, row=2, sticky=E)
-label_path.pack()
-
 button = Button(mainframe, text="Select Folder", command=select_folder)
 button.grid(column=2, row=2, sticky=W)
-button.pack()
+
+filepath = StringVar()
+label_path = Label(mainframe, textvariable=filepath).grid(column=1, row=2, sticky=E)
 
 
 ttk.Button(mainframe, text='Converter', command=assembling).grid(column=3, row=3, sticky=W)
